a3c2c084a9ffdda89ef41f34900cf0ee9b436023
StudentOfJS <rodlewis45@gmail.com>
2018-05-07 17:18:37 +0200

    email service integration - signup confirmation

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,5 +1,6 @@
 import * as bcrypt from "bcryptjs"
 import * as yup from "yup"
+import { v4 } from 'uuid'
 import { ResolverMap } from "../../types/graphql-utils"
 import { User } from "../../entity/User"
 import { formatYupError } from "../../utils/formatYupError"
@@ -10,6 +11,7 @@ import {
   passwordNotLongEnough
 } from "./errorMessages"
 import { createConfirmEmailLink } from "../../utils/createConfirmEmailLink"
+import { sendEmail } from "../../utils/sendEmail";
 
 const schema = yup.object().shape({
   email: yup
@@ -57,13 +59,13 @@ export const resolvers: ResolverMap = {
 
       const hashedPassword = await bcrypt.hash(password, 10)
       const user = User.create({
+        id: v4(),
         email,
         password: hashedPassword
       })
 
       await user.save()
-
-      await createConfirmEmailLink(url, user.id, redis)
+      process.env.NODE_ENV !== "test" ? await sendEmail(email, await createConfirmEmailLink(url, user.id, redis)) : null
 
       return null
     }
