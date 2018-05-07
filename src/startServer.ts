371eb67b838ac27595fc7a7432604d4c6dbd5366
StudentOfJS <rodlewis45@gmail.com>
2018-05-07 09:28:51 +0200

    seperated confirmEmail route and added test

--- src/startServer.ts
+++ src/startServer.ts
@@ -4,10 +4,10 @@ import * as path from 'path'
 import * as fs from 'fs'
 import { mergeSchemas, makeExecutableSchema } from 'graphql-tools'
 import { GraphQLSchema } from 'graphql'
-import * as Redis from 'ioredis'
 
+import { redis } from './redis'
 import { createTypeormConn } from "./utils/createTypeormConn"
-import { User } from './entity/User';
+import { confirmEmail } from './routes/confirmEmail';
 
 export const startServer = async () => {
 
@@ -19,7 +19,7 @@ export const startServer = async () => {
     return makeExecutableSchema({ resolvers, typeDefs })
   })
 
-  const redis = new Redis()
+
 
   const server = new GraphQLServer({
     schema: mergeSchemas({ schemas }),
@@ -29,17 +29,7 @@ export const startServer = async () => {
     })
   })
 
-  server.express.get("/confirm/:id", async (req, res) => {
-    const { id } = req.params
-    const userId = await redis.get(id)
-    if (userId) {
-      await User.update({ id: userId }, { confirmed: true })
-      await redis.del(id)
-      res.send("ok")
-    } else {
-      res.send("invalid")
-    }
-  })
+  server.express.get("/confirm/:id", confirmEmail)
 
   await createTypeormConn()
   const app = await server.start({
--- src/utils/createConfirmEmailLink.test.ts
+++ src/utils/createConfirmEmailLink.test.ts
@@ -49,12 +49,5 @@ describe('test createConfirmEmailLink', () => {
     const value = await redis.get(key)
     expect(value).toBeNull()
   })
-
-  test("sends invalid back if bad id sent", async () => {
-    const response = await fetch(`${process.env.TEST_HOST}/confirm/12083`)
-    const text = await response.text()
-    expect(text).toEqual("invalid")
-  })
-
 })
 
